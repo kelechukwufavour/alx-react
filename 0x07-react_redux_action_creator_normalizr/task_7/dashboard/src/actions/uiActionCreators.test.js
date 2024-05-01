3675f0d47198ea3eb5f0267982409868663d076c
kelechukwufavour <kelechukwufavour.faye@gmail.com>
2024-05-01 13:41:44 +0100

    Async Action Creators

--- 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
@@ -1,15 +1,20 @@
 import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from "./uiActionTypes";
-
 import { login, logout, hideNotificationDrawer, displayNotificationDrawer } from "./uiActionCreators";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import fetchMock from "fetch-mock";
+
+const middleWares = [thunk];
+const mockStore = configureStore(middleWares);
 
 describe("tests for UI notification action creators", () => {
   it("should create proper action for login", () => {
-    const email = "james@gmail.com";
-    const password = "heheheh";
+    const email = "charles@gmail.com";
+    const password = "Oxygen";
 
     expect(login(email, password)).toEqual({
       type: LOGIN,
-      user: { email: "james@gmail.com", password: "heheheh" },
+      user: { email: "charles@gmail.com", password: "Oxygen" },
     });
   });
 
@@ -28,4 +33,46 @@ describe("tests for UI notification action creators", () => {
       type: HIDE_NOTIFICATION_DRAWER,
     });
   });
+
+  // it('should pass LOGIN and LOGIN_SUCCESS to the store if API returns the right response', () => {
+  // 	fetchMock.get('http://localhost:8564/login-success.json', {
+  // 		first_name: 'Malcom',
+  // 		last_name: 'Doe',
+  // 		email: 'malcom.doe@alxafrica.nz',
+  // 		profile_picture: 'http://placehold.it/32x32',
+  // 	});
+
+  // 	const store = mockStore({});
+  // 	const email = 'malcom.doe@alxafrica.nz';
+  // 	const password = 'password';
+
+  // 	return store.dispatch(
+  // 		loginRequest(email, password).then(() => {
+  // 			const actions = store.getActions();
+  // 			console.log(actions);
+  // 			expect(actions[0]).toEqual(loginSuccess());
+  // 			expect(actions[1]).toEqual(login());
+  // 		})
+  // 	);
+  // 	fetchMock.restore();
+  // });
+
+  // it('should pass LOGIN and LOGIN_FAILURE if API query fails', () => {
+  // 	fetchMock.get('http://localhost:8564/login-success.json', 400);
+
+  // 	const store = mockStore({});
+  // 	const email = 'malcom.doe@alxafrica.nz';
+  // 	const password = 'password';
+
+  // 	return store.dispatch(
+  // 		loginRequest(email, password).then(() => {
+  // 			const actions = store.getActions();
+  // 			console.log(actions);
+  // 			expect(actions[0]).toEqual(loginFailure());
+  // 			expect(actions[1]).toEqual(login());
+  // 		})
+  // 	);
+
+  // 	fetchMock.restore();
+  // });
 });
