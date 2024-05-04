7e9561196eaf0d47e28e0f8c04616dc9a5b0f2d6
kelechukwufavour <kelechukwufavour.faye@gmail.com>
2024-05-04 10:01:17 +0100

    Create a reducer for Courses

--- 0x08-react_redux_reducer_selector/task_0/dashboard/src/actions/courseActionTypes.js
+++ 0x08-react_redux_reducer_selector/task_0/dashboard/src/actions/courseActionTypes.js
@@ -1,2 +1,4 @@
 export const SELECT_COURSE = "SELECT_COURSE";
 export const UNSELECT_COURSE = "UNSELECT_COURSE";
+// actions/courseActionTypes.js
+export const FETCH_COURSE_SUCCESS = 'FETCH_COURSE_SUCCESS';
