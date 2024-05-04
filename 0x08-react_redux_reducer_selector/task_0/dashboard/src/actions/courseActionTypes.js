67d30216c4039ccea3b4783aec2081fd67d8dba8
kelechukwufavour <kelechukwufavour.faye@gmail.com>
2024-05-04 10:14:37 +0100

    Create the reducer for notifications

--- 0x08-react_redux_reducer_selector/task_0/dashboard/src/actions/courseActionTypes.js
+++ 0x08-react_redux_reducer_selector/task_0/dashboard/src/actions/courseActionTypes.js
@@ -2,3 +2,7 @@ export const SELECT_COURSE = "SELECT_COURSE";
 export const UNSELECT_COURSE = "UNSELECT_COURSE";
 // actions/courseActionTypes.js
 export const FETCH_COURSE_SUCCESS = 'FETCH_COURSE_SUCCESS';
+// actions/notificationActionTypes.js
+export const FETCH_NOTIFICATIONS_SUCCESS = 'FETCH_NOTIFICATIONS_SUCCESS';
+export const MARK_AS_READ = 'MARK_AS_READ';
+export const SET_TYPE_FILTER = 'SET_TYPE_FILTER';
