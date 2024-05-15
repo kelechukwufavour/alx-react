781ceda90eb11c296ab76ec1eb7120a973c3b1ff
kelechukwufavour <kelechukwufavour.faye@gmail.com>
2024-05-15 19:59:13 +0100

    making progress

--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import App from "./App/App";
 import uiReducer, { initialState } from "./reducers/uiReducer";
 import { Map } from "immutable";
 
-const store = createStore(uiReducer, Map(initialState), applyMiddleware(thunk));
+// const store = createStore(
+//   uiReducer,
+//   Map(initialState),
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  uiReducer,
+  Map(initialState),
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
